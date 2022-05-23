a35ec0158611b21b02354336b3c7fa7f1b52b717
figuesco <ada_hack317@hackademy.mx>
2022-05-23 17:31:29 -0500

    Class aplicante
    escribiendo codigo

--- api/academicprofile/models.py
+++ api/academicprofile/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class aplicante(models.Model):
+    codigo = models.CharField(max_length=3, primary_key= True)
+    nombre = models.CharField(max_length=50)
+    nivel = models.CharField(max_length=50)
+    institución = models.CharField(max_length=50)
+    duración = models.CharField(max_length=50)
+    Estatus = models.CharField(max_length=50)
