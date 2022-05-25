d3fa2e7c785c818d6685bb8ba71c218a0f458869
figuesco <ada_hack317@hackademy.mx>
2022-05-25 13:47:43 -0500

    changes in documents
    Userprofile por academicprofile

--- api/academicprofile/url.py
+++ api/academicprofile/url.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.authtoken import views
 
-from academicprofile.views import UserProfileView
-from academicprofile.models import UserProfile
+from academicprofile.views import academicprofileView
+from academicprofile.models import academicprofile
 
 urlpatterns =[
     path('auth/academicprofile/', academicprofileView.as_view()),
