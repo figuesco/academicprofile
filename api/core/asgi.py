a2e68f62a60f0eb6b4aee1d40b569d7401468824
figuesco <ada_hack317@hackademy.mx>
2022-07-05 16:15:17 -0500

    cambios asgi

--- api/core/asgi.py
+++ api/core/asgi.py
@@ -14,3 +14,12 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.base')
 
 application = get_asgi_application()
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    })
